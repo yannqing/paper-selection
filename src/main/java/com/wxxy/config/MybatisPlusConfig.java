5d7f27697661db7c94697402de1c4a21af300221
yannqing <67121851@qq.com>
2024-02-01 19:03:04 +0800

    修复bug

--- src/main/java/com/wxxy/config/MybatisPlusConfig.java
+++ src/main/java/com/wxxy/config/MybatisPlusConfig.java
@@ -8,7 +8,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@MapperScan("com.wxxy.mapper")
+//@MapperScan("com.wxxy.mapper")
 public class MybatisPlusConfig {
 
     /**
