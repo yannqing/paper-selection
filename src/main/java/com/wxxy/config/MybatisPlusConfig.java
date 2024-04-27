b1c80b03f16743d03043ea216286f5ddb3ec1728
yannqing <67121851@qq.com>
2024-04-27 18:34:01 +0800

    修复bug

--- src/main/java/com/wxxy/config/MybatisPlusConfig.java
+++ src/main/java/com/wxxy/config/MybatisPlusConfig.java
@@ -3,7 +3,6 @@ package com.wxxy.config;
 import com.baomidou.mybatisplus.annotation.DbType;
 import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
 import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
-import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -20,13 +19,4 @@ public class MybatisPlusConfig {
         //interceptor.addInnerInterceptor(new PaginationInnerInterceptor()); 如果有多数据源可以不配具体类型 否则都建议配上具体的DbType
         return interceptor;
     }
-
-//    lookup
-//        @Bean
-//    public MybatisPlusInterceptor mybatisPlusInterceptor() {
-//        xxasdfasdfasdfinterceptor = new MybatisPlusInterceptor();
-//        interceptor.addInnerInterceptor(new PaginationInnerInterceptor(DbType.MYSQL));//如果配置多个插件,切记分页最后添加
-//        //interceptor.addInnerInterceptor(new PaginationInnerInterceptor()); 如果有多数据源可以不配具体类型 否则都建议配上具体的DbType
-//        return interceptor;
-//    }
 }
